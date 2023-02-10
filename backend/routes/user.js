ca94eeb5c081a4438ae65ac7b20805018bfe56d5
Rebekah Grace <rmgrace@ucdavis.edu>
2023-02-10 01:28:53 -0800

    this does not work, but it has  bunch of code that's useful

--- backend/routes/user.js
+++ backend/routes/user.js
@@ -14,18 +14,30 @@ router.route("/").get((req, res) => {
 router.route("/create_user").post((req, res) => {
   const req_name = req.body.name;
   const req_email = req.body.email;
+  const req_username = req.body.username;
 
   // console.log(req.body)
 
   const newUser = new User({
     name: req_name,
-    email: req_email
+    email: req_email,
+    username: req_username
   });
 
   newUser
     .save()
     .then(() => res.json("new User added"))
     .catch((err) => res.status(400).json("Error: " + err));
+});
+
+router.route('/check_exist_username/:id').get(async (req, res) => {
+
+  const userExist = await User.findOne({ username: req["params"]["id"] });
+  let valid = true;
+  if (userExist === null){
+    valid = false;
+  }
+  return res.json(valid);
 });
 
 router.route('/check_exist/:id').get(async (req, res) => {
--- frontend/tread/src/App.js
+++ frontend/tread/src/App.js
@@ -5,19 +5,40 @@ import axios from 'axios';
 
 function App() {
   const [ user, setUser ] = useState([]);
-  const [ profile, setProfile ] = useState([]);
-  const [ newProfile, setNewProfile] = useState(false)
+  const [ profile, setProfile ] = useState(false);
+  const [ chooseUsername, setChooseUsername] = useState(false);
+  const [ newProfile, setNewProfile] = useState(false);
+  const [ usernameSubmitted, setUsernameSubmitted] = useState(false);
 
+  let state = {"text":"", "username":""};
 
+  /* Google Auth functions */
+  // Login
   const login = useGoogleLogin({
     onSuccess: (codeResponse) => setUser(codeResponse),
     onError: (error) => console.log('Login Failed:', error)
   });
 
-  function generateUsername(){
-    return "45";
+  // log out function to log the user out of google and set the profile array to null
+  const logOut = () => {
+    googleLogout();
+    setProfile(null);
+  };
+
+  /* Username Form information */
+  // Set the text state to the desired username
+  function inputUsername(text){
+    state["text"] = text.target.value;
   }
 
+  // The username has been submitted, begin checking if it is valid
+  function submitUsername(e){
+    setUsernameSubmitted(true);
+    e.preventDefaults();
+  }
+
+  /* Logged in now, set the profile information and if the user is new, open the submittal form */
+  // Check the User's email has been already used
   async function checkUserAlreadyExists(email){
     var config = {
       method: 'get',
@@ -38,7 +59,7 @@ function App() {
     return exist;
   }
 
-
+  // Log in the user
   useEffect(
     () => {
       if (user) {
@@ -51,10 +72,12 @@ function App() {
           })
           .then(async (res) => {
             setProfile(res.data);
-            console.log("Do I exist? " + await checkUserAlreadyExists(res.data.email));
             if( ! await checkUserAlreadyExists(res.data.email) ){
-              console.log("Am i trying to set a new profile?");
-              setNewProfile(true)
+              console.log("The user does not exist and must be added");
+              setChooseUsername(true);
+            }
+            else{
+              console.log("The user exists.");
             }
           })
           .catch((err) => console.log(err));
@@ -63,12 +86,60 @@ function App() {
     [ user ]
   );
 
+  /* Determine if the username is valid */
+  function convertInt4DigitString(integer){
+    let string = integer.toString();
+
+    while (string.length < 4){
+      string = "0" + string;
+    }
+
+    return string;
+  }
+
+  async function findValidUsername(username_base, base_digit_code){
+    let username_found = true;
+    let digit_string = convertInt4DigitString(base_digit_code);
+    let attempt_username = username_base + "#" + digit_string;
+
+    while (username_found){
+      digit_string = convertInt4DigitString(base_digit_code);
+      attempt_username = username_base + "#" + digit_string;
+
+      var config = {
+        method: 'get',
+        url: 'http://localhost:5000/user/check_exist_username/' + attempt_username ,
+        headers: {
+          'Content-Type': 'application/json'
+        }
+      };
+
+      await axios(config)
+        .then(function (response) {
+          username_found = response.data;
+        })
+        .catch(function (error) {
+          console.log("erorr trying to check if the username exists");
+          console.log(error);
+        });
+
+      base_digit_code = (base_digit_code + 1) % 9999;
+    }
+
+    console.log("Decided on this username: " + attempt_username);
+    state["username"] = attempt_username;
+    return attempt_username;
+  }
+
+
+
   useEffect(
     () => {
       if (newProfile) {
         var data = JSON.stringify({
           "name" : profile.name,
-          "email" : profile.email
+          "email" : profile.email,
+          "username": state["username"]
         });
         console.log(data);
 
@@ -93,11 +164,6 @@ function App() {
     }
   );
 
-  // log out function to log the user out of google and set the profile array to null
-  const logOut = () => {
-    googleLogout();
-    setProfile(null);
-  };
 
   return (
     <div>
@@ -113,6 +179,12 @@ function App() {
           <br />
           <br />
           <button onClick={logOut}>Log out</button>
+
+          {(chooseUsername) ? <form  id = "userForm" >
+        <label>Select a Username</label>
+        <input onChange = {inputUsername} type = "text" name = "UsernamePicker" id = "UsernamePicker"></input>
+        <button type = "submit" value = "Next"></button>
+        </form> : <></>}
         </div>
       ) : (
         <button onClick={() => login()}>Sign in with Google 🚀 </button>
