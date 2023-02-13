df252794f7d2bd8a482cb5a5e4ad1a482dd450d4
Rebekah Grace <rmgrace@ucdavis.edu>
2023-02-13 03:04:34 -0800

    add images

--- backend/models/user.model.js
+++ backend/models/user.model.js
@@ -1,6 +1,5 @@
 const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
-
 const userSchema = new Schema(
   {
     name: String,
@@ -8,7 +7,7 @@ const userSchema = new Schema(
     username : String,
     sent_requests: [{source: String, dest: String}],
     recieved_requests: [{source: String, dest: String}],
-    friends: [String]
+    friends: [String],
   },
   {
     collection: "user_info"
