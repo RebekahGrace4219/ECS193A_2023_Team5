cebf8dc5c3d57ce2471ebda0687793db5b863782
Rebekah Grace <rmgrace@ucdavis.edu>
2023-02-17 23:31:47 -0800

    frontend started

--- frontend/tread/src/components/ProfileHeader.js
+++ frontend/tread/src/components/ProfileHeader.js
@@ -1,21 +1,84 @@
 import "../css/profileHeader.css"
-
+import React, {useState} from 'react';
 const ProfileHeader = () => {
+
+
+    function getName(){
+        return "Rebekah Grace";
+    }
+
+    function getUsername(){
+        return "@BronzeTiger#4567";
+    }
+
+    function getProfilePhoto(){
+        return "https://i.imgur.com/2BMQKKi.png";
+    }
+
+
+    function getNumberLeagues(){
+        let number = 0;
+        let stringResponse = "0";
+
+        if (number >= 10){
+            stringResponse = "9+";
+        }
+        else{
+            stringResponse = number.toString();
+        }
+
+        return stringResponse;
+    }
+
+    function getNumberFriends(){
+        let number = 3;
+        let stringResponse = "0";
+
+        if (number >= 10){
+            stringResponse = "9+";
+        }
+        else{
+            stringResponse = number.toString();
+        }
+
+        return stringResponse;
+    }
+
+    function getNumberMedals(){
+        let number = 0;
+        let stringResponse = "0";
+
+        if (number >= 10){
+            stringResponse = "9+";
+        }
+        else{
+            stringResponse = number.toString();
+        }
+
+        return stringResponse;
+    }
+
+    const [name, setName] = useState(getName());
+    const [username, setUsername] = useState(getUsername());
+    const [profilePhoto, setProfilePhoto] = useState(getProfilePhoto());
+    const [numberLeagues, setNumberLeagues] = useState(getNumberLeagues());
+    const [numberFriends, setNumberFriends] = useState(getNumberFriends());
+    const [numberMedals, setNumberMedals] = useState(getNumberMedals());
 
     return (
         <div className = "profileHeaderClass">
             <div className = "profileNamePhoto">
                 <h1 id = "profileName">Profile</h1>
-                <div id = "changePhoto"><img id = "profilePhoto" src = "https://i.imgur.com/2BMQKKi.png" alt = "profile"/></div>
+                <div id = "changePhoto"><img id = "profilePhoto" src = {profilePhoto} alt = "profile"/></div>
             </div>
             <div className = "displaySection">
                 <div id = "blankSpace"></div>
-                <div id = "displayName">Rebekah Grace</div>
-                <div id = "displayUsername">@BronzeTiger#4219</div>
+                <div id = "displayName">{name}</div>
+                <div id = "displayUsername">{username}</div>
                 <div className="displayUserInfo">
-                    <div className = "numberInfo">3 Leagues</div>
-                    <div className = "numberInfo">34 Friends</div>
-                    <div className = "numberInfo">14 Medals</div>
+                    <div className = "numberInfo">{numberLeagues} Leagues</div>
+                    <div className = "numberInfo">{numberFriends} Friends</div>
+                    <div className = "numberInfo">{numberMedals} Medals</div>
                 </div>
             </div>
         </div>
