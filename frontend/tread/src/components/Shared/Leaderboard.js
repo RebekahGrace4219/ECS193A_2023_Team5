6d3a34458a619d95f280198acf4c5c81952b9833
Rebekah Grace <rmgrace@ucdavis.edu>
2023-03-02 00:00:03 -0800

    add leaderboard information

--- frontend/tread/src/components/Shared/Leaderboard.js
+++ frontend/tread/src/components/Shared/Leaderboard.js
@@ -1,6 +1,19 @@
+import LeaderboardEntry from "./LeaderboardEntry";
 
-const Leaderboard = () => {
-    return (<div></div>);
+const Leaderboard = (props) => {
+    function makeLeaderboardEntryObj(input){
+        return (<LeaderboardEntry>{input}</LeaderboardEntry>)
+    }
+    return(
+        <div id = "Leaderboard">
+            <h2>{props.children.title}</h2>
+
+            <div>
+                {props.children.entries.map(makeLeaderboardEntryObj)}
+            </div>
+        </div>
+
+    );
 }
 
 export default Leaderboard;
--- frontend/tread/src/components/Shared/LeaderboardEntry.js
+++ frontend/tread/src/components/Shared/LeaderboardEntry.js
@@ -1,6 +1,22 @@
 
-const LeaderboardEntry = () => {
-    return (<div></div>);
+const LeaderboardEntry = (props) => {
+    return (
+    <div id = "LeaderboardEntry">
+        <div>
+            {(props.children.level === 1) ? <img src = "https://i.imgur.com/m5iDo4X.png"/>: <></>}
+            {(props.children.level === 1) ? <img src = "https://i.imgur.com/QctK692.png"/>: <></>}
+            {(props.children.level === 1) ? <img src = "https://i.imgur.com/QaT2m92.png"/>: <></>}
+            {(props.children.level > 3) ? <p>{props.children.level}</p>: <></>}
+        </div>
+        <div>
+            <img src={props.children.photo}/>
+            <p>{props.children.displayName}</p>
+            <ProgressBar>{{"children":{props.children.complete}}}</ProgressBar>
+            <img src= "https://i.imgur.com/lfvWyWP.png"/>
+            <p>{props.children.score}</p>
+        </div>
+    </div>
+    );
 }
 
 export default LeaderboardEntry;
