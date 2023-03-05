057436ca82903bd48feac71b6dd85673742d535b
Rebekah Grace <rmgrace@ucdavis.edu>
2023-03-04 21:17:06 -0800

    recent activity box css

--- src/components/Social/ActivityObj.js
+++ src/components/Social/ActivityObj.js
@@ -4,11 +4,13 @@ const ActivityObj = (props) => {
     let challengeType = props.children.challengeType;
     let time = props.children.time;
     let challengeTitle = props.children.challengeTitle;
-
+    let photo = props.children.photo;
     return (
-        <div>
-            <img src = "https://i.imgur.com/jJaPs4q.png"></img>
-            <p>{displayName} {progress} on a {challengeType} Challenge - {challengeTitle}</p>
+        <div id = "ActivityObj">
+            <div id = "ActivityLeftSide">
+                <img id = "activityProfilePhoto" src = {photo}></img>
+                <p className = "activityObjText">{displayName} {progress} on a {challengeType} Challenge - {challengeTitle}</p>
+            </div>
             <p>{time}</p>
         </div>
     );
