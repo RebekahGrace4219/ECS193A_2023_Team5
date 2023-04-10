c0a3e885b0ee73e487e8ba5f06dddfc384498e90
Rebekah Grace <rmgrace@ucdavis.edu>
2023-04-09 20:23:37 -0700

    added request to friends in suggested friends

--- src/components/Social/SuggestedFriendObj.js
+++ src/components/Social/SuggestedFriendObj.js
@@ -1,6 +1,31 @@
 import "../../css/Shared/suggestionBox.css";
-
+import axios from "axios";
+const backend_url = process.env.REACT_APP_PROD_BACKEND;
 const SuggestedFriendObj = (props) => {
+    async function sendFriendRequest(){
+
+        var config = {
+            method : 'post',
+            url : backend_url + 'friend_list/send_friend_request',
+            headers: {
+              Accept: 'application/json',
+            },
+            data :
+            {
+              friendName : props.username
+            },
+            withCredentials: true,
+            credentials: 'include'
+          };
+          axios(config)
+          .then(function(response) {
+          })
+          .catch(function(error){
+              console.log(error)
+              console.log("No response")
+          });
+    }
+
     return (
         <div className="suggestionObj">
             <div className = "suggestionImageSection">
@@ -9,6 +34,7 @@ const SuggestedFriendObj = (props) => {
             <div className = "suggestionWritingSection">
                 <div  className = "suggestionHeaderStyle">{props.username}</div>
                 <div  className = "suggestionTextStyle">{props.mutualFriends} Mutual Friends</div>
+                <button className = "submitButton" onClick = {sendFriendRequest}><p className = "submitButtonText"> Add Friend</p></button>
             </div>
         </div>
     );
