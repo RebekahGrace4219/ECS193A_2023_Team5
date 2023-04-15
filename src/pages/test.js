f16f2c4b3453e9b454ee29f831552d3118b05a5e
Rebekah Grace <rmgrace@ucdavis.edu>
2023-04-14 20:40:29 -0700

    add button move

--- src/pages/test.js
+++ src/pages/test.js
@@ -1,58 +1,30 @@
 import React, {useState} from 'react';
-
-import ChallengeScroll from "../components/Challenge/ChallengeScroll";
+import CloudinaryUploadWidget from "./CloudinaryUploadWidget";
 import "../css/Shared/page3.css"
 
 const TestDiv = () => {
-    let total = 100;
-    let props = {"children":{"progress":{"Bruce Wayne":10, "Diana Prince":40, "Clark Kent":60},
-    "exerciseType":"Run",
-    "unit":"km",
-    "amount":100,
-    "dueDate":"2023-03-10",
-    "username":"Bruce Wayne",
-    "photos":{
-        "Bruce Wayne": "https://i.imgur.com/E2x8xyY.png",
-        "Clark Kent": "https://i.imgur.com/q3vP5BH.png",
-        "Diana Prince":"https://i.imgur.com/3Ia9gVG.png"},
-    "sentUser": "Clark Kent",
-    "receivedUser": "Justice League",
-    "type": "league"}};
-
-    function sortProgress(a, b){
-        return -1*(a.complete - b.complete);
-    }
-
-    function min(a,b){
-        if (a<b){
-            return a;
-        }
-        return b;
-    }
-    function makeLeaderboardObj(){
-        let keys = Object.keys(props.children.progress);
-        let leaderBoardInfo = [];
-        for (let i = 0; i < keys.length; i++){
-            let person = keys[i];
-            let personObj = {};
-            personObj["photo"] = props.children.photos[person];
-            personObj["displayName"] = person;
-            personObj["complete"] = min(100, props.children.progress[person] / total * 100);
-            personObj["score"] = props.children.progress[person];
-            leaderBoardInfo.push(personObj);
-        }
-
-        leaderBoardInfo.sort(sortProgress);
-
-        for (let i = 0; i < keys.length; i++){
-            leaderBoardInfo[i]["level"] = i+1;
-        }
-        return leaderBoardInfo;
-    }
 
     return(
         <div id = "test">
-            <ChallengeScroll type = "issued" ifLeague = {false} leagueID = {""}></ChallengeScroll>
+            <h3>Cloudinary Upload Widget Example</h3>
+            <CloudinaryUploadWidget />
+            <p>
+                <a
+                href="https://cloudinary.com/documentation/upload_widget"
+                target="_blank"
+                >
+                Upload Widget User Guide
+                </a>
+            </p>
+            <p>
+                <a
+                href="https://cloudinary.com/documentation/upload_widget_reference"
+                target="_blank"
+                >
+                Upload Widget Reference
+                </a>
+            </p>
+            <img id="uploadedimage" src=""></img>
         </div>
     );
 }
