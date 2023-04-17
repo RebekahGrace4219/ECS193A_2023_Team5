1bed1f9c253625358d78406b57ccd3988dbf7ca9
Rebekah Grace <rmgrace@ucdavis.edu>
2023-04-16 19:51:46 -0700

    udpate photo upload div

--- src/components/Shared/PhotoUpload.js
+++ src/components/Shared/PhotoUpload.js
@@ -3,8 +3,49 @@ import '../../css/Shared/photoUpload.css';
 
 const backend_url = process.env.REACT_APP_PROD_BACKEND;
 
+
+
 const PhotoUpload = (props) => {
-    const [imageSrc, setImageSrc] = useState(props.defaultImage);
+    const [image, setImage] = useState(props.defaultImage);
+
+    function getBase64(file) {
+        var reader = new FileReader();
+        reader.readAsDataURL(file);
+        reader.onload = function () {
+          // Set the viewing image
+          setImage(reader.result);
+
+          // send the image up the stream
+          props.func(reader.result);
+        };
+        reader.onerror = function (error) {
+          console.log('Error: ', error);
+        };
+     }
+
+    function onImageChange (event) {
+        // Select the photo
+        let photo = event.target.files[0];
+        getBase64(photo);
+
+    }
+
+    return (
+        <div>
+            <div className = "photoShow">
+                <img id = "uploadProfilePicture" className = "loadedProfileImage" src = {image} alt = "profile"></img>
+            </div>
+            <input className = "uploadPhoto" type = "file" accept = "image/*" onChange = {onImageChange}></input>
+        </div>
+    );
+
+
+
+}
+
+
+/*
+const [imageSrc, setImageSrc] = useState(props.defaultImage);
 
 
     function onImageChange(event) {
@@ -26,5 +67,5 @@ const PhotoUpload = (props) => {
             <input className = "uploadPhoto" type = "file" accept = "image/*" onChange = {onImageChange}></input>
         </div>
     );
-}
+*/
 export default PhotoUpload;
