1d0a1c7a36c9d3a6d4d5f0830951a6fc5c2e301d
Rebekah Grace <rmgrace@ucdavis.edu>
2023-04-16 17:24:08 -0700

    fixed compile error

--- src/components/Profile/ProfileHeader.js
+++ src/components/Profile/ProfileHeader.js
@@ -2,9 +2,12 @@ import {useState, useEffect} from 'react';
 
 const ProfileHeader = () => {
     const [load, setLoad] = useState(false);
+    const [username, setUsername] = useState("");
+    const [displayName, setDisplayName] = useState("");
+    const [qrcode, setQRCode] = useState("");
+    const [profilePhoto, setProfilePhoto] = useState("");
     return(
         <div>
-            <img src = {profilePicture}/>
             <p>{username}</p>
             <p>{displayName}</p>
             <p>{qrcode}</p>
