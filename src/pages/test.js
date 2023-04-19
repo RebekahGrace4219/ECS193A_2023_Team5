a84792c416a960c512286d2e8eec43958cf9cc73
Rebekah Grace <rmgrace@ucdavis.edu>
2023-04-18 21:04:44 -0700

    set up skeleton

--- src/pages/test.js
+++ src/pages/test.js
@@ -3,28 +3,26 @@ import CloudinaryUploadWidget from "./CloudinaryUploadWidget";
 import "../css/Shared/page3.css"
 
 const TestDiv = () => {
+    const [exerciseLog, setExerciseLog] = useState([]);
+    const [challengeLog, setChallengeLog] = useState([]);
+
+    const requestExercises = () => {
+        setExerciseLog([]);
+    }
+
+    const requestChallenges = () => {
+        setChallengeLog([]);
+    }
+
+    const determineExercises = () => {
+        // Convert the exercise log values into
+        //(exercise, amount, unit)
+    }
+
 
     return(
-        <div id = "test">
-            <h3>Cloudinary Upload Widget Example</h3>
-            <CloudinaryUploadWidget />
-            <p>
-                <a
-                href="https://cloudinary.com/documentation/upload_widget"
-                target="_blank"
-                >
-                Upload Widget User Guide
-                </a>
-            </p>
-            <p>
-                <a
-                href="https://cloudinary.com/documentation/upload_widget_reference"
-                target="_blank"
-                >
-                Upload Widget Reference
-                </a>
-            </p>
-            <img id="uploadedimage" src=""></img>
+        <div>
+
         </div>
     );
 }
