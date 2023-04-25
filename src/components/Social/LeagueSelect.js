665f00b57607f40eedddba6cee09f934471819df
Rebekah Grace <rmgrace@ucdavis.edu>
2023-04-24 22:00:08 -0700

    condense and correct logic league select

--- src/components/Social/LeagueSelect.js
+++ src/components/Social/LeagueSelect.js
@@ -1,11 +1,18 @@
 import "../../css/Shared/form.css";
 
 const LeagueSelect = (props) => {
+    let role = props.role;
+    let type = props.type;
     return(
         <div>
-            <select className = "formSelect" onChange = {props.leagueReact}>
-            <option value="none" selected disabled hidden>Select an Option</option>
-                <option value = "leave">Leave League</option>
+            <select className = "formSelect" onChange = {props.leagueReact} defaultValue = "none">
+            <option value="none" disabled hidden></option>
+            {(role === "admin" || role === "participant") ? <option value = "leave">Leave League</option> : <></>}
+            {(role === "owner") ? <option value = "delete">Delete League</option> : <></>}
+            {(role === "admin") ? <option value = "remove">Remove Self as Admin</option> : <></>}
+            {(type === "sent") ? <option value = "revoke">Revoke request</option> : <></>}
+            {(type === "invite") ? <option value = "accept">Accept Invite</option> : <></>}
+            {(type === "invite") ?<option value = "decline">Decline Invite</option> : <></>}
             </select>
         </div>
     );
