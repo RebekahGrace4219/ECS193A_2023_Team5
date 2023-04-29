6e22fd0dda3086054ce9f7c6a4a9979b94ab2aba
Rebekah Grace <rmgrace@ucdavis.edu>
2023-04-28 17:59:06 -0700

    make medals selection bar

--- src/components/Profile/MedalsSection.js
+++ src/components/Profile/MedalsSection.js
@@ -1,29 +1,23 @@
 import {useState} from 'react';
-import MedalsBar from './MedalsBar';
+import Bar from '../Shared/Bar';
 import MedalsScroll from './MedalsScroll';
 import "../../css/Shared/section.css";
 import "../../css/Shared/bar.css";
 
 const MedalsSection = () => {
-    const [ifEarned, setIfEarned] = useState(true);
-
+    const [ifEarned, setIfEarned] = useState("Earned");
+    let buttonList = [{"name": "Earned", "defaultOn":true, "create":false},
+    {"name": "In Progress", "defaultOn":false, "create":false},];
     return (
     <div id = "MedalsSection" className='section'>
         <div id = "MedalsHeader" className ="selectButtonHeader">
             <h1>Medals</h1>
-            <MedalsBar changeFunction = {setIfEarned}></MedalsBar>
+            <Bar>{{"buttonList":buttonList, "updateFunc":setIfEarned}}</Bar>
         </div>
 
-        { ifEarned ?
-            <MedalsScroll>{{"type" : "earned"}}</MedalsScroll>
-            :
-            <></>
-        }
+        { ifEarned=== "Earned" ? <MedalsScroll>{{"type" : "earned"}}</MedalsScroll> : <></> }
+        { ifEarned ==="In Progress" ? <MedalsScroll>{{"type" : "progress"}}</MedalsScroll>:<></>}
 
-        {!ifEarned ?
-            <MedalsScroll>{{"type" : "progress"}}</MedalsScroll>
-            :<></>
-        }
     </div>
     );
 }
